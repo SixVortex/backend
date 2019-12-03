83a5788ccd8b5f5c9958cfa4488355aa772bca4f
Adrian Klasson <31548090+Gimanua@users.noreply.github.com>
2019-12-03 13:30:59 +0100

    Completed skeleton of project

---
+++ src/main/java/org/sustaining/sustaining_backend/beans/ImageBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sustaining.sustaining_backend.beans;
+
+import javax.ejb.Stateless;
+import org.sustaining.sustaining_backend.entities.Image;
+
+/**
+ *
+ * @author Elev
+ */
+@Stateless
+public class ImageBean {
+    
+    public Image getImage(int imageID){
+        return null;
+    }
+    
+    public Image postImage(Image image){
+        return null;
+    }
+}
