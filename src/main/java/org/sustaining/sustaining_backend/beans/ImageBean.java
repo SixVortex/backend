f0bdca42ecc85a8da17292ecb20cf3c093a5c52e
Lea <leagossen@gmail.com>
2019-12-04 09:43:24 +0100

    Image Beans

--- src/main/java/org/sustaining/sustaining_backend/beans/ImageBean.java
+++ src/main/java/org/sustaining/sustaining_backend/beans/ImageBean.java
@@ -5,7 +5,13 @@
  */
 package org.sustaining.sustaining_backend.beans;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
+import org.sustaining.sustaining_backend.ConnectionFactory;
 import org.sustaining.sustaining_backend.entities.Image;
 
 /**
@@ -14,6 +20,29 @@ import org.sustaining.sustaining_backend.entities.Image;
  */
 @Stateless
 public class ImageBean {
+    
+       public List<Image> getimage ( int imageID) {
+           try (Connection connection = ConnectionFactory.getConnection()){
+               
+               String sql = "SELECT * FROM image WHERE image_id=?";
+               PreparedStatement stmt = connection.prepareStatement(sql);
+               stmt.setInt(1, imageID);
+               
+               ResultSet result = stmt.executeQuery();
+               List<Image> images = new ArrayList();
+               while(result.next()){
+                   int date = result.getInt("date");
+                   int title = result.getInt("title");
+                   int location = result.getInt("location");
+                   int image = result.getInt("image");
+                   
+               }
+               return images;
+           } catch (Exception ex) {
+               return null;
+           }
+          
+       }
     
     public Image getImage(int imageID){
         return null;
