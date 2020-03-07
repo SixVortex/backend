35942476bcece14e6587003803c7c347d54f2427
erikh <erikh@DESKTOP-4MTUUBP>
2020-03-07 11:02:28 +0100

    Removed auto generated comments and added some base javadoc

--- src/main/java/org/sustaining/sustaining_backend/resources/PostResource.java
+++ src/main/java/org/sustaining/sustaining_backend/resources/PostResource.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.sustaining.sustaining_backend.resources;
 
 import javax.ejb.EJB;
@@ -12,44 +7,58 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
 import org.sustaining.sustaining_backend.beans.PostBean;
 
 /**
  * This class contains all the resources for posts.
+ *
  * @author Adrian
  */
 @Path("")
 @Produces(MediaType.APPLICATION_JSON)
 public class PostResource {
-    
+
     @EJB
     PostBean postBean;
-    
-    @GET
-    @Path("posts/all/{numberOfPosts}")
+
     /**
      * This function is automatically called by the server when a GET request is sent to
      * the specified Path. It parses the amount of posts to get and returns them in a Response.
+     *
+     * @param numberOfPosts
+     * @return
      */
-    public Response getPosts(@PathParam("numberOfPosts") int numberOfPosts){
+    @GET
+    @Path("posts/all/{numberOfPosts}")
+    public Response getPosts(@PathParam("numberOfPosts") int numberOfPosts) {
         return postBean.getPosts(numberOfPosts);
     }
-    
+
+    /**
+     * Gets the a number of posts with fame rating
+     * @param numberOfPosts Number of posts to get
+     * @return
+     */
     @GET
     @Path("posts/fame/{numberOfPosts}")
-    public Response getFamePosts(@PathParam("numberOfPosts") int numberOfPosts){
+    public Response getFamePosts(@PathParam("numberOfPosts") int numberOfPosts) {
         return postBean.getFamePosts(numberOfPosts);
     }
-    
+
+    /**
+     * @param numberOfPosts
+     * @return
+     */
     @GET
     @Path("posts/shame/{numberOfPosts}")
-    public Response getShamePosts(@PathParam("numberOfPosts") int numberOfPosts){
+    public Response getShamePosts(@PathParam("numberOfPosts") int numberOfPosts) {
         return postBean.getShamePosts(numberOfPosts);
     }
-    
+
     @GET
     @Path("post/{lastImageID}")
-    public Response getNextPost(@PathParam("lastImageID") int lastImageID){
+    public Response getNextPost(@PathParam("lastImageID") int lastImageID) {
         return postBean.getNextPost(lastImageID);
     }
 }
